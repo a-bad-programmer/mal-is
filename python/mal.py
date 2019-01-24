8c4dad1f4ff18e0ad7926265b7af7f8d627f8703
a-bad-programmer <quintinf19@parkschool.net>
2019-01-24 14:33:35 -0500

    step0

---
+++ python/mal.py
@@ -0,0 +1,24 @@
+import sys
+
+def READ(x):
+    return x
+
+def EVAL(x):
+    return x
+
+def PRINT(x):
+    return x
+
+def rep(x):
+    return READ(EVAL(PRINT(x)))
+
+def repl():
+    try:
+        x = input()
+
+        print(rep(x))
+    except EOFError:
+        exit()
+
+while True:
+    repl()
